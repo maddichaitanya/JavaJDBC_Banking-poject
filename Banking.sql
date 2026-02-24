23595bed9c495f727d9d97f970635b88e2729013
Maddi Chaitanya <150575674+maddichaitanya@users.noreply.github.com>
2026-02-24 10:28:52 +0530

    Revise database schema for banking application
    Modified account and transactions tables to include contact validation and foreign key constraint.

--- Banking.sql
+++ Banking.sql
@@ -1,19 +1,24 @@
 CREATE DATABASE bankdb;
 
-USE bankdb;
+use bankdb;
 
 CREATE TABLE account (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50),
-    contact BIGINT UNIQUE,
+    contact VARCHAR(10) UNIQUE,
     pin INT,
-    balance DOUBLE
+    balance DOUBLE,
+    CHECK (contact REGEXP '^[0-9]{10}$')
 );
 
 CREATE TABLE transactions (
     tid INT PRIMARY KEY AUTO_INCREMENT,
-    contact BIGINT,
+	name VARCHAR(50),
+    contact VARCHAR(10) UNIQUE,
     type VARCHAR(20),
     amount DOUBLE,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (contact) REFERENCES account(contact)
 );
+
+drop database bankdb; 
